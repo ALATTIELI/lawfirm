f53b78de02eae3f6734dd1d12941c9e45f7c5543
ADEL ALATTIELI <2010181@acuq.ae>
2023-09-03 15:40:30 +0400

    the bigining

---
+++ src/App.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <About />
+      <Services />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <nav>
+      <h1>Law Firm Name</h1>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contactus">Contact Us</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+
+function Header() {
+  return (
+    <header id="home">
+      <h2>Welcome to Our Law Firm</h2>
+      <p>We provide the best legal services in town.</p>
+    </header>
+  );
+}
+
+function About() {
+  return (
+    <section id="about">
+      <h2>About Us</h2>
+      <p>Our firm has been serving the community for over 20 years...</p>
+    </section>
+  );
+}
+
+function Services() {
+  return (
+    <section id="services">
+      <h2>Our Services</h2>
+      <ul>
+        <li>Personal Injury</li>
+        <li>Corporate Law</li>
+        <li>Family Law</li>
+        // ... add other services here
+      </ul>
+    </section>
+  );
+}
+
+function Contact() {
+  return (
+    <section id="contactus">
+      <h2>Contact Us</h2>
+      
+      {/* Contact Information */}
+      <div className="contact-info">
+        <h3>Contact Information</h3>
+        <p>Email: contact@lawfirm.com</p>
+        <p>Phone: +1-123-456-7890</p>
+        <p>Address: 123 Law Firm St., City, Country</p>
+      </div>
+
+      {/* Callback Form */}
+      <div className="callback-form">
+        <h3>Request a Callback</h3>
+        <form>
+          <div className="input-group">
+            <label>Name:</label>
+            <input type="text" name="name" required />
+          </div>
+          <div className="input-group">
+            <label>Phone Number:</label>
+            <input type="tel" name="phone" required />
+          </div>
+          <div className="input-group">
+            <label>Message:</label>
+            <textarea name="message" rows="4" required></textarea>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2023 Law Firm Name. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default App;
